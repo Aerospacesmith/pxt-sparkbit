219cc0d9f0ddd7df591041b347380163bc2dc620
Aerospacesmith <keanan_smith@hotmail.com>
2021-08-25 23:10:32 -0500

    Update sparkbit.ts

--- sparkbit.ts
+++ sparkbit.ts
@@ -413,11 +413,11 @@ namespace sparkbitI {
     //% $inputPort.fieldEditor="gridpicker"
     //% $inputPort.fieldOptions.width=220
     //% $inputPort.fieldOptions.columns=8
-    //% $inputPort.fieldOptions.decompileLiterals=true
     //% parts=”v2"
     export function __sparkbitInPortEnum(inputPort: SparkbitInPort): SparkbitInPort {
         return inputPort;
     }
+ 
     //% blockId=Input1
     //% block="input 1"
     //% blockHidden=true
@@ -425,6 +425,7 @@ namespace sparkbitI {
     export function __Input1(): SparkbitInPort {
         return SparkbitInPort.Input1;
     }
+ 
     //% blockId=Input2
     //% block="input 2"
     //% blockHidden=true
@@ -432,6 +433,7 @@ namespace sparkbitI {
     export function __Input2(): SparkbitInPort {
         return SparkbitInPort.Input2;
     }
+ 
     //% blockId=Input3
     //% block="input 3"
     //% blockHidden=true
@@ -439,6 +441,7 @@ namespace sparkbitI {
     export function __Input3(): SparkbitInPort {
         return SparkbitInPort.Input3;
     }
+ 
     //% blockId=Input4
     //% block="input 4"
     //% blockHidden=true
@@ -446,6 +449,7 @@ namespace sparkbitI {
     export function __Input4(): SparkbitInPort {
         return SparkbitInPort.Input4;
     }
+ 
     //% blockId=Input5
     //% block="input 5"
     //% blockHidden=true
@@ -453,13 +457,15 @@ namespace sparkbitI {
     export function __Input5(): SparkbitInPort {
         return SparkbitInPort.Input5;
     }
+ 
     //% blockId=Input6
     //% block="input 6"
     //% blockHidden=true
     //% parts=”v2"
     export function __Input6(): SparkbitInPort {
         return SparkbitInPort.Input6;
-    }    
+    }   
+ 
     //% blockId=Input7
     //% block="input 7"
     //% blockHidden=true
@@ -467,6 +473,7 @@ namespace sparkbitI {
     export function __Input7(): SparkbitInPort {
         return SparkbitInPort.Input7;
     }
+ 
     //% blockId=Input8 
     //% block="input 8"
     //% blockHidden=true
