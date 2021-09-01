187eba5ed58a4083a0ed05998eee08a099ce3a01
Aerospacesmith <keanan_smith@hotmail.com>
2021-08-31 20:08:08 -0500

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,22 +1,28 @@
 {
-    // rotate motor on output #1 500 ms clockwise
+    // rotate motor on output #1 clockwise for 500 ms non-blocking
     sparkbitO.rotateMotorModule(SparkbitOutPort.Output1, SparkbitDirection.Clockwise, 100, 500)
-    basic.pause(100);
-    sparkbitO.stopMotorModule(SparkbitOutPort.Output1);
 
-    // set light on output #2 red for 500 ms
-    sparkbitO.setLightModule(SparkbitOutPort.Output2, SparkbitColor.Red, 100, 500);
-    basic.pause(100);
-    sparkbitO.stopLightModule(SparkbitOutPort.Output2);
+    //rotate motor on output #2 Counterclockwise for 2000 ms blocking program until complete
+    sparkbitO.rotateMotorModule(SparkbitOutPort.Output2, SparkbitDirection.Counterclockwise, 100)
+    basic.pause(2000);
+    sparkbitO.stopMotorModule(SparkbitOutPort.Output2);
+
+    // set light on output #3 red for 3000 ms non-blocking
+    sparkbitO.setLightModule(SparkbitOutPort.Output3, SparkbitColor.Red, 100, 3000)
+
+    //set light on output #4 green for 5000 ms blocking program until complete
+    sparkbitO.setLightModule(SparkbitOutPort.Output4, SparkbitColor.Green, 100)
+    basic.pause(5000)
+    sparkbitO.stopLightModule(SparkbitOutPort.Output4)
 
     // Play V2 audio on speaker
-    //soundExpression.giggle.playUntilDone();
+    //soundExpression.giggle.playUntilDone()
 
     // Play V1 audio on speaker
-    //music.startMelody(music.builtInMelody(Melodies.Dadadadum));
+    //music.startMelody(music.builtInMelody(Melodies.Dadadadum))
 
     // Read sensor #1
-    basic.showNumber(sparkbitI.analogSensorPercent(SparkbitInPort.Input1));
+    basic.showNumber(sparkbitI.analogSensorPercent(SparkbitInPort.Input1))
 
 
 }
