9639cb3e1e340d24df8242ea663c27a404548345
Aerospacesmith <keanan_smith@hotmail.com>
2021-08-31 19:32:39 -0500

    Update sparkbit.ts

--- sparkbit.ts
+++ sparkbit.ts
@@ -167,6 +167,7 @@ namespace sparkbitI {
     //% weight=200
     //% channel.shadow="input" channel.defl=SparkbitInPort.Input1
     //% parts="v2"
+    //% help=KidSpark/pxt-sparkbit#bump-sensor-blue
     export function bumpSensorIsPressed(channel: number): boolean {
         return readDigitalSensorBool(channel);
     }
